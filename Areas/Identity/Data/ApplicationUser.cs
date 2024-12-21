f56618b7a6d6b83318a4b3798dd983bc2d214830
Stefan Adams <138134496+stefowizz@users.noreply.github.com>
2024-12-21 18:52:22 -0500

    Add files via upload

---
+++ Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace YardyRiver.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    [Column(TypeName ="nvarchar(100)")]
+    [PersonalData]
+    public string FirstName { get; set; }
+
+    [Column(TypeName = "nvarchar(100)")]
+    [PersonalData] 
+    public string LastName { get; set; } 
+}
+
