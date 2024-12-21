a26f8923e8fcb84c606f56034ff7039fc286382c
Stefan Adams <138134496+stefowizz@users.noreply.github.com>
2024-12-21 18:48:28 -0500

    Add files via upload

---
+++ Models/DbSeeder.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using YardyRiver.Areas.Identity.Data;
+
+namespace YardyRiver.Models
+{
+    public class DbSeeder
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public DbSeeder(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager; 
+        }
+
+        public async Task SeedRolesAndAdminAsync()
+        {
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole { Name = "Admin" });
+            }
+
+            if (await roleManager.FindByNameAsync("user") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole { Name = "User" });
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                FirstName = "Admin",
+                LastName = "Adams",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+
+            var userInDb = await userManager.FindByEmailAsync(user.Email);
+            if (userInDb == null)
+            {
+                await userManager.CreateAsync(user, "Admin@123");
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+        }
+
+    }
+}
