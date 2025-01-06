9bd3442e12646f79e126fc5d2c567bf8a344d778
Stefan Adams <138134496+stefowizz@users.noreply.github.com>
2025-01-06 09:24:01 -0500

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,3 +1,6 @@
+
+
+
 let slideIndex = 0;
 showSlides();
 
@@ -7,9 +10,12 @@ function showSlides() {
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
 
+
+
     for (i = 0; i < slides.length; i++) {
 
-        slides[i].style.display = "none"; let slideIndex = 0;
+        slides[i].style.display = "none"; 
+        let slideIndex = 0;
         showSlides();
 
         function showSlides() {
@@ -20,7 +26,7 @@ function showSlides() {
 
             for (i = 0; i < slides.length; i++) {
 
-                slides[i].style.display = "none";
+                slides[i].style.display = "none";   
 
             }
 
@@ -42,8 +48,10 @@ function showSlides() {
 
             dots[slideIndex - 1].className += " active";
 
-            setTimeout(showSlides, 3000); // Change image every 3 seconds
+            setTimeout(showSlides, 5000); // Image appears every 7 second. 
         }
+
+
 
 
         var video = document.getElementById("myVideo");
@@ -62,42 +70,42 @@ function showSlides() {
 
         //<button id="myBtn" onclick="myFunction()">Pause</button><br /> //place this tag in the INDEX file. 
 
-    }
+    } 
 
-    slideIndex++;
-
-    if (slideIndex > slides.length) {
-
-        slideIndex = 1
-
-    }
-
-    for (i = 0; i < dots.length; i++) {
-
-        dots[i].className = dots[i].className.replace(" active", "");
-
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-
-    dots[slideIndex - 1].className += " active";
-
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
 } 
 
 
-var video = document.getElementById("myVideo");
-var btn = document.getElementById("myBtn");
 
-function myFunction() {
 
-    if (video.paused) {
-        video.play();
-        //btn.innerHTML = "Pause";
-    } else {
-        video.pause();
-        //btn.innerHTML = "Play";
-    }
-}
+
+
+//This is a replica of the conditional statement above inside the for loop.
+
+
+
+
+//slideIndex++;
+
+//if (slideIndex > slides.length) {
+
+//slideIndex = 1
+
+//}
+
+//for (i = 0; i < dots.length; i++) {
+
+//dots[i].className = dots[i].className.replace(" active", "");
+
+//}
+
+//slides[slideIndex - 1].style.display = "block";
+
+//dots[slideIndex - 1].className += " active";
+
+//setTimeout(showSlides, 3000); // Change image every 3 seconds
+
+//var video = document.getElementById("myVideo");
+//var btn = document.getElementById("myBtn"); 
+
 
 //<button id="myBtn" onclick="myFunction()">Pause</button><br /> //place this tag in the INDEX file. 
